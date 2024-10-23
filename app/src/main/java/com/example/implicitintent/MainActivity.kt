ed899dc9a381b22dd60ff9c879821739edbca379
rakesh <rakeshbeesetty53@gmail.com>
2024-10-23 12:11:06 +0530

    Added logic for implicit intent

--- app/src/main/java/com/example/implicitintent/MainActivity.kt
+++ app/src/main/java/com/example/implicitintent/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.implicitintent
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -15,6 +19,19 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val browserCard = findViewById<CardView>(R.id.browserCard)
+        val cameraCard = findViewById<CardView>(R.id.cameraCard)
+
+        browserCard.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://github.com/Beesettyrakesh?tab=repositories")
+            startActivity(intent)
+        }
+
+        cameraCard.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
         }
     }
 }
